6290e63e589f0292ccf5e6312d4b9d70985fc5d0
Allan <allanlealluz@gmail.com>
2026-03-10 10:32:05 -0300

    AACI-54 Adicionar constraints nos DTOs (ex: @NotNull, @NotBlank, @Email, @Size)

--- src/main/java/com/senai/monsai/application/dto/AtualizarSenhaDTO.java
+++ src/main/java/com/senai/monsai/application/dto/AtualizarSenhaDTO.java
@@ -1,8 +1,12 @@
 package com.senai.monsai.application.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
-@Data
-public class AtualizarSenhaDTO {
-    private String novaSenha;
+public record AtualizarSenhaDTO (
+
+    @NotBlank(message = "A nova senha é obrigatória.")
+    String novaSenha
+){
+
 }
